9e9b95a9c3936aae3a6e784077f2ceec3250b1ab
Josie <josiecollinsrc@gmail.com>
2021-02-15 16:31:26 -0600

    Added local storage function to each hour, added class changes depending on hour, started README

--- Assets/script.js
+++ Assets/script.js
@@ -7,10 +7,43 @@ $(document).ready(function(){
         localStorage.setItem(time, wordVal);
         
     });
-    // Target id & description, add value of localStorage-getItem to that hour
+
+    // Target id & description, add value of localStorage.getItem to that hour
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
     // Conditional to update colors of hours
+    function colorUpdator() {
+        var currentHour = moment().hours();
+        console.log(currentHour);
+        $(".time-block").each( function() {
+            var blockHour = parseInt($(this).attr("id"));
+            if (blockHour < currentHour) {
+                $(this).addClass("past")
+            }
+            else if (blockHour === currentHour) {
+                $(this).addClass("present")
+            }
+            else {
+                $(this).addClass("future")
+            }
+        })
+    }
 
-    // Add current time (line 29) set text to date
+    colorUpdator();
+
+
+    // Post the current time to the page
+    var currentDay = document.querySelector("#currentDay");
+    var currentTime = moment();
+    currentDay.append(currentTime.format("MMMM Do YYYY, dddd, h:mm A"));
+
 });
 //Don't do anything below
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 05-Day-Planner
 Day planner assignment
+
+# Why it was Made
+This website was made in order to help people who require an online planner for their workday. From the hours of 9 to 5, this planner gives the user the ability to set tasks for certain hours and save them to their local storage, so they can easily check what tasks are left. It also tells the user when it is time for that task through the use of moment.js and class changes of the hours. It also lets the user know when the time is past for a certain task. It is overall very useful for organizational purposes and very helpful for work.
+
+# 
